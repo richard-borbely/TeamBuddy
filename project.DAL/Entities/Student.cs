c7b340f193d4840e939d732bf51c2655226dd968
Igor Mjasojedov <xmjaso00@stud.fit.vutbr.cz>
2019-03-10 22:26:44 +0100

    Collection of posts added to Students class

--- project.DAL/Entities/Student.cs
+++ project.DAL/Entities/Student.cs
@@ -10,5 +10,6 @@ namespace project.DAL.Entities
         public string name { get; set; }
         public string passwd { get; set; }
         public ICollection<Team> teams { get; set; }
+        public ICollection<Post> posts { get; set; }
     }
 }
