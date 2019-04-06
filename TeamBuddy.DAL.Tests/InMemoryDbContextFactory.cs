79b9944d66865f57299e8fe74cbcdb54c31db78b
Igor Mjasojedov <xmjaso00@stud.fit.vutbr.cz>
2019-04-06 03:13:08 +0200

    .DAL first test created

---
+++ TeamBuddy.DAL.Tests/InMemoryDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TeamBuddy.DAL.Tests
+{
+    public class InMemoryDbContextFactory : IDbContextFactory
+    {
+        public TeamBuddyDbContext CreateDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TeamBuddyDbContext>();
+            optionsBuilder.UseInMemoryDatabase("TeamBuddyDb");
+            return new TeamBuddyDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ TeamBuddy.DAL.Tests/TeamBuddyDbContextTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using TeamBuddy.DAL.Entities;
+using TeamBuddy.DAL.Enumerations;
+
+
+namespace TeamBuddy.DAL.Tests
+{
+    public class TeamBuddyDbContextTests
+    {
+        private readonly IDbContextFactory dbContextFactory;
+
+        public TeamBuddyDbContextTests()
+        {
+            dbContextFactory = new InMemoryDbContextFactory();
+        }
+
+
+        [Fact]
+        public void CreateUserTest()
+        {
+            //Arrange
+            var user = new User
+            {
+                Name = "Dimitrij Orlov",
+                Gender = Gender.Male,
+                Email = "orlov.dima@gmail.com",
+                Status = Status.Offline
+            };
+
+            //Act
+            using (var dbContext = dbContextFactory.CreateDbContext())
+            {
+                dbContext.Users.Add(user);
+                dbContext.SaveChanges();
+            }
+               
+            //Assert
+            using (var dbContext = dbContextFactory.CreateDbContext())
+            {
+                User retrievedUser = null;
+                try
+                {
+                    retrievedUser = dbContext.Users.First(t => t.Id == user.Id);
+                    Assert.NotNull(retrievedUser);
+                }
+                finally
+                {
+                    //Teardown
+                    if (retrievedUser != null)
+                    {
+                        dbContext.Users.Remove(retrievedUser);
+                    }
+                }
+            }
+
+
+        }
+
+
+    }
+}
