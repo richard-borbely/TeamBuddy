3c34cd47917ba2bd8bf5ce1ac12048f951111bdf
Igor Mjasojedov <xmjaso00@stud.fit.vutbr.cz>
2019-04-06 04:00:13 +0200

    [.DAL] new tests added

--- TeamBuddy.DAL.Tests/TeamBuddyDbContextTests.cs
+++ TeamBuddy.DAL.Tests/TeamBuddyDbContextTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
 using TeamBuddy.DAL.Entities;
@@ -54,10 +55,105 @@ namespace TeamBuddy.DAL.Tests
                     }
                 }
             }
-
-
         }
 
+        [Fact]
+        public void CreateTeamTest()
+        {
+            //Arrange
+            var team = new Team
+            {
+                Name = "TeamBoddy",
+                Description = "The first team of this app."
+            };
 
+            //Act
+            using (var dbContext = dbContextFactory.CreateDbContext())
+            {
+                dbContext.Teams.Add(team);
+                dbContext.SaveChanges();
+            }
+
+            //Assert
+            using (var dbContext = dbContextFactory.CreateDbContext())
+            {
+                Team retrievedTeam = null;
+                try
+                {
+                    retrievedTeam = dbContext.Teams.First(t => t.Id == team.Id);
+                    Assert.NotNull(retrievedTeam);
+                }
+                finally
+                {
+                    //Teardown
+                    if (retrievedTeam != null)
+                    {
+                        dbContext.Teams.Remove(retrievedTeam);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public void CreatePostTest()
+        {
+            //Arrange
+            var post = new Post
+            {
+                Title = "Our First Post",
+                Text = "Hello World! This is just testing post.",
+                Time_of_post = DateTime.Now,
+                User = new User
+                {
+                    Name = "Dimitrij Orlov",
+                    Gender = Gender.Male,
+                    Email = "orlov.dima@gmail.com",
+                    Status = Status.Offline
+                },
+                Team = new Team
+                {
+                    Name = "TeamBoddy",
+                    Description = "The first team of this app."
+                }
+            };
+
+            //Act
+            using (var dbContext = dbContextFactory.CreateDbContext())
+            {
+                dbContext.Posts.Add(post);
+                dbContext.SaveChanges();
+            }
+
+            //Assert
+            using (var dbContext = dbContextFactory.CreateDbContext())
+            {
+                Post retrievedPost = null;
+                try
+                {
+                    retrievedPost = dbContext.Posts
+                        .Include(p => p.User)
+                        .Include(p => p.Team)
+                        .First(p => p.Id == post.Id);
+                    Assert.NotNull(retrievedPost);
+                    Assert.NotNull(retrievedPost.User);
+                    Assert.NotNull(retrievedPost.Team);
+                    var retrievedUser = dbContext.Users
+                        .First(u => u.Id == post.User.Id);
+                    Assert.NotNull(retrievedUser);
+                    var retrievedTeam = dbContext.Teams
+                        .First(t => t.Id == post.Team.Id);
+                    Assert.NotNull(retrievedTeam);
+                }
+                finally
+                {
+                    //Teardown
+                    if (retrievedPost != null)
+                    {
+                        dbContext.Remove(retrievedPost);
+                    }
+                }
+            }
+        }
+    
     }
 }
