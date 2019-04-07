d1590a87c1132a3373ceb230944963d468c806cb
Igor Mjasojedov <xmjaso00@stud.fit.vutbr.cz>
2019-04-07 10:11:04 +0200

    Services/Hashing Class of hash function for hashing passwords DONE

---
+++ TeamBuddy.DAL/Services/Hasher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace TeamBuddy.DAL.Services
+{
+    public class Hasher : IHasher
+    {
+        public string GetHash(string input)
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            string hash = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: input,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 10000,
+                numBytesRequested: 256));
+            return hash;
+        }
+    }
+}
---
+++ TeamBuddy.DAL/Services/IHasher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamBuddy.DAL.Services
+{
+    public interface IHasher
+    {
+        string GetHash(string input);
+    }
+}
